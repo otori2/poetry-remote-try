29c13449509ac146c6b066296139ec11cf1f7683
otori <otori@example.com>
2021-10-17 03:29:29 +0000

    Template for Fast API

---
+++ app.py
@@ -0,0 +1,14 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    msg = "hoge"
+    return {"hello world": msg}
+
+
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
